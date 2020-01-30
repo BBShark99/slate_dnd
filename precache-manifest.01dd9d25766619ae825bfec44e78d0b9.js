9bc5a429ee27d1f2d35cb687adecca9c23a37f92
bbshark99 <blue.shark99@outlook.com>
2020-01-30 12:30:39 +1000

    Updates

---
+++ precache-manifest.01dd9d25766619ae825bfec44e78d0b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f432ebaec85443e8f940ede8260da955",
+    "url": "/slate_dnd/index.html"
+  },
+  {
+    "revision": "60e15462114f5b3d3124",
+    "url": "/slate_dnd/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "60e15462114f5b3d3124",
+    "url": "/slate_dnd/static/js/2.3ba43b42.chunk.js"
+  },
+  {
+    "revision": "66f285a2802b28461de92e189b8b0e33",
+    "url": "/slate_dnd/static/js/2.3ba43b42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2be12e1fcd04ea1641b1",
+    "url": "/slate_dnd/static/js/main.523e7289.chunk.js"
+  },
+  {
+    "revision": "19dce21ab6649fd56249",
+    "url": "/slate_dnd/static/js/runtime-main.5258b6f4.js"
+  }
+]);
